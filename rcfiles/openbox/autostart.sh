0d955a2bc16ada010fdce4810340f8c28080f2bb
Ranjit Mathew <rmathew@gmail.com>
2014-05-22 22:02:24 +0530

    Tweaks to display.

--- rcfiles/openbox/autostart.sh
+++ rcfiles/openbox/autostart.sh
@@ -1,6 +1,9 @@
 # This shell script is run before Openbox launches.
 # Environment variables set here are passed to the Openbox session.
 
+# Set a keyboard repeat-delay of 500 ms and a repeat-rate of 50 hz.
+/usr/bin/xset r rate 500 50
+
 # Set a background color
 # BG=/usr/bin/xsetroot
 # test -z $BG || $BG -solid SteelBlue
