2859600e159b24cd46957a6e7c67d1bff5ce3e5d
Ranjit Mathew <rmathew@gmail.com>
2015-08-07 19:06:41 +0530

    Fix fbpanel configuration.

--- rcfiles/openbox/autostart.sh
+++ rcfiles/openbox/autostart.sh
@@ -11,19 +11,7 @@
 
 # D-bus
 if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
-       eval `dbus-launch --sh-syntax --exit-with-session`
-fi
-
-# # Preload stuff for KDE apps
-# if which start_kdeinit >/dev/null; then
-# LD_BIND_NOW=true start_kdeinit --new-startup +kcminit_startup &
-# fi
-
-# Run XDG autostart things.  By default don't run anything desktop-specific
-# See xdg-autostart --help more info
-DESKTOP_ENV="OPENBOX"
-if which /usr/lib/openbox/xdg-autostart >/dev/null; then
-  /usr/lib/openbox/xdg-autostart $DESKTOP_ENV
+  eval `dbus-launch --sh-syntax --exit-with-session`
 fi
 
 if test -x /usr/bin/fbpanel >/dev/null; then
