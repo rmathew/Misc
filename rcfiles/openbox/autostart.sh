113464cc685fd951fac50be1d90d1b27e9287239
Ranjit Mathew <rmathew@gmail.com>
2021-11-14 19:30:18 +0530

    Update configurator for CMake.

--- rcfiles/openbox/autostart.sh
+++ rcfiles/openbox/autostart.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+#
 # This shell script is run before Openbox launches.
 # Environment variables set here are passed to the Openbox session.
 
 # Set a keyboard repeat-delay of 500 ms and a repeat-rate of 50 hz.
 /usr/bin/xset r rate 500 50
 
-# Set a background color
+# Set a background color or wallpaper.
 # BG=/usr/bin/xsetroot
 # test -z $BG || $BG -solid SteelBlue
+# /usr/bin/feh --bg-tile --no-fehbg "/home/rmathew/lucifer.jpg"
 /usr/bin/xsetroot -solid "#002550"
 
 # D-bus
@@ -14,6 +17,6 @@ if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
   eval `dbus-launch --sh-syntax --exit-with-session`
 fi
 
-if test -x /usr/bin/fbpanel >/dev/null; then
+if [[ -x /usr/bin/fbpanel ]]; then
   /usr/bin/fbpanel &
 fi
